538e94f43f98a65e9e7771ee1b3653ebcf6e5d0d
Nicolò ruggeri <nicolo.ruggeri@studenti.unicam.it>
2017-09-28 19:47:26 +0200

    basta

--- js/indexUserScript.js
+++ js/indexUserScript.js
@@ -45,7 +45,7 @@ var userMovements = null;
 //user alerts
 var userAlerts = null;
 //link heroku
-var serverheroku = "https://bancaunicambackend.herokuapp.com";
+var serverheroku = "https://bancaunicambackend.herokuapp.com/";
 //user home controller
 /**
  * Dato che questo è il primo controller utilizzato richiamerò tutte le funzioni del server per salvare
